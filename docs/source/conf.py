facafed396c0abaefb5fa954977da01b7b63a45c
Vinay Hegde <19818985+vinayhegde1990@users.noreply.github.com>
2024-05-26 19:31:06 +0530

    Modify DevOps Notes User Interface
    Why this change?
    - Change theme to Sphinx Book v1.1.2 with light/dark mode support on desktop and mobile devices
    - Upgrade version and copyright details
    
    Resolves: #12

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,12 +20,11 @@
 # -- Project information -----------------------------------------------------
 
 project = u'DevOps-Notes'
-copyright = u'2023-2024, Vinay Hegde'
+copyright = u'2024-2025, Vinay Hegde'
 author = u'Vinay Hegde'
 
 # The full version, including alpha/beta/rc tags
-release = u'2.0.23'
-
+release = u'3.0.24'
 
 # -- General configuration ---------------------------------------------------
 
@@ -75,7 +74,7 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_book_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
